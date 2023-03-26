d9f65ad406834ca452c7fcb95a806ce5ac625ba0
phuong-tanila <nguyenphuong040217@gmail.com>
2023-03-26 21:46:09 +0700

    add checkout api

--- Course_Online_Backend/src/main/java/fa/training/backend/controller/OrderController.java
+++ Course_Online_Backend/src/main/java/fa/training/backend/controller/OrderController.java
@@ -1,10 +1,19 @@
 package fa.training.backend.controller;
 
+import fa.training.backend.entities.Course;
 import fa.training.backend.entities.Order;
+import fa.training.backend.entities.OrderDetail;
+import fa.training.backend.entities.User;
+import fa.training.backend.exception.RecordNotFoundException;
 import fa.training.backend.mapper.OrderMapper;
+import fa.training.backend.model.ExceptionResponse;
 import fa.training.backend.model.OrderModel;
+import fa.training.backend.services.CourseService;
 import fa.training.backend.services.OrderService;
 import fa.training.backend.util.SortOrder;
+import java.security.Principal;
+import java.util.Date;
+import java.util.HashSet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -14,6 +23,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
+import javax.annotation.security.RolesAllowed;
+import org.springframework.security.core.Authentication;
 
 @RestController
 @RequestMapping("/order")
@@ -24,7 +36,11 @@ public class OrderController {
     public SortOrder sortOrder;
     @Autowired
     public OrderMapper orderMapper;
+    @Autowired
+    public CourseService courseService;
 
+    
+    @RolesAllowed("US")
     @GetMapping("/{userId}")
     public ResponseEntity<List<OrderModel>> getCourseByCategory(@PathVariable("userId") int userId,
                                                                 @RequestParam(defaultValue = "0") Integer pageNo,
@@ -43,5 +59,46 @@ public class OrderController {
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+    @RolesAllowed("US")
+    @PostMapping("/check-out")
+    public ResponseEntity checkOut(
+            Principal principal,
+            @RequestBody List<Integer> courseIds,
+            @RequestParam(defaultValue = "") String paymentId,
+            @RequestParam(defaultValue = "false") boolean paymentStatus,
+            @RequestParam(defaultValue = "") String coupon,
+            @RequestParam(defaultValue = "cod") String paymentMethod
+    ){
+        User currentUser = (User) ((Authentication) principal).getPrincipal();
+        Order order = new Order();
+        order.setCoupon(coupon);
+        order.setPaymentId(paymentId);
+        order.setPaymentStatus(paymentStatus);
+        order.setBuyDate(new Date());
+        order.setPaymentMethod(paymentMethod);
+        order.setOrderDetails(new HashSet<>());
+        order.setUser(currentUser);
+        for (Integer courseId : courseIds) {
+            try {
+                Course course = courseService.findById(courseId);
+                OrderDetail orderDetail = new OrderDetail();
+                orderDetail.setCourse(course);
+                orderDetail.setPrice(course.getTuitionFee());
+                System.out.println(order);
+                order.getOrderDetails().add(orderDetail);
+            } catch (RecordNotFoundException ex) {
+                return new ResponseEntity<ExceptionResponse>(
+                        new ExceptionResponse(
+                                "RecordNotFoundException",
+                                "One of course ids passed in is not found!"
+                        ),
+                        HttpStatus.BAD_REQUEST
+                );
+            }
+        }
+        orderService.saveOrder(order);
+        System.out.println(order);
+        return new ResponseEntity(HttpStatus.CREATED);
     }
 }
